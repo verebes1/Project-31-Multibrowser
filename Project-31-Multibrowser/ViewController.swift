aa94eafca0f10f96cee51ba1d1852f38f9d32e02
David Verbenyi <dverbenyi@gmail.com>
2019-03-06 10:24:04 +0000

    Initial Commit

---
+++ Project-31-Multibrowser/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  Project-31-Multibrowser
+//
+//  Created by verebes on 12/02/2019.
+//  Copyright © 2019 A&D Progress. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegate, UIGestureRecognizerDelegate {
+
+    @IBOutlet weak var addressBar: UITextField!
+    @IBOutlet weak var stackView: UIStackView!
+    
+    weak var activeWebView: WKWebView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setDefaultTitle()
+        
+        let add = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addWebView))
+        let delete = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteWebView))
+        navigationItem.rightBarButtonItems = [delete, add]
+        
+    }
+
+
+    private func setDefaultTitle() {
+        title = "Multibrowser"
+    }
+    
+    @objc private func addWebView() {
+        let webView = WKWebView()
+        webView.navigationDelegate = self
+        
+        stackView.addArrangedSubview(webView)
+        
+        let url = URL(string: "https://www.apple.com")!
+        webView.load(URLRequest(url: url))
+        
+        webView.layer.borderColor = UIColor.blue.cgColor
+        selectWebView(webView)
+        
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(webViewTapped))
+        recognizer.delegate = self
+        webView.addGestureRecognizer(recognizer)
+        
+    }
+    
+    @objc private func deleteWebView() {
+        
+    }
+    
+    private func selectWebView(_ webView: WKWebView) {
+        for view in stackView.arrangedSubviews {
+            view.layer.borderWidth = 0
+        }
+        activeWebView = webView
+        webView.layer.borderWidth = 3
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("EDIT MODE ON")
+        if let webView = activeWebView, let address = addressBar.text {
+            print("WE ARE TRYING TO GET AN URL")
+            if let url = URL(string: address) {
+                print("WE ARE LOADING")
+                webView.load(URLRequest(url: url))
+            }
+        }
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @objc private func webViewTapped(_ recognizer: UITapGestureRecognizer) {
+        if let selectedWebView = recognizer.view as? WKWebView {
+            selectWebView(selectedWebView)
+        }
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+}
+
